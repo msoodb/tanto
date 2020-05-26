c9df5afa71b6303021e018ed1f41d221cc612967
Alireza Davari <alireza.davari.h@gmail.com>
2020-05-26 23:49:57 +0430

    add tanto_unittest

--- tests/tanto_unittest.c
+++ tests/tanto_unittest.c
@@ -4,18 +4,61 @@
 
 #include "../tanto.h"
 #include "./acutest.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-int add(int a, int b)
+TJSON_t* parse_sample_json()
 {
-  return a + b;
+  char *stream;
+  stream = tanto_read_file("tests/sample.json");
+  TJSON_t *json = NULL;
+  TANTO_INIT(&json);
+
+  tanto_parse(&json, stream);
+  return json;
 }
 
-void test_add(void)
+char* find_value(char *key)
 {
-  TEST_CHECK_(add(2, 2) == (2 + 2), "add(%d, %d)==%d", 2, 2, (2+2));
+  TJSON_t *json = parse_sample_json();
+
+  char *fin =  tanto_find(json, key)->value;
+  return fin;
+}
+
+TJSON_t* get_object(char *key)
+{
+  TJSON_t *json = parse_sample_json();
+  TJSON_t *fin =  tanto_find(json, key);
+
+  if (fin != NULL)
+  {
+    return fin->child;
+  }
+
+  return fin;
+}
+
+void tests(void)
+{
+  TEST_CHECK_(strcmp(find_value("root"), "mine") == 0, "find_value(\"root\")");
+  TEST_CHECK_(strcmp(find_value("parser"), "tanto") == 0, "find_value(\"parser\")");
+  TEST_CHECK_(strcmp(find_value("name"), "Awesome 4K") == 0, "find_value(\"name\")");
+  TEST_CHECK_(strcmp(find_value("private"), "true") == 0, "find_value(\"private\")");
+
+  // TODO : handle multiple datatypes.
+  TEST_CHECK_(strcmp(find_value("version"), "1") == 0, "find_value(\"version\")");
+
+  // Check objects
+  TEST_CHECK_(get_object("versions") != NULL , "get_object(\"versions\")");
+
+  // TODO: we need another method for arrays
+  TEST_CHECK_(get_object("functions") != NULL , "get_object(\"functions\")");
 }
 
 TEST_LIST = {
-  {"int add(int, int);", test_add},
+  {"int tests();", tests},
   {0}
 };
